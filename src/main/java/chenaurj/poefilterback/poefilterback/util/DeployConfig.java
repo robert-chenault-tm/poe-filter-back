5ca017cccfce3985b05db2d3a313f5fc5fd98ca2
robert-chenault-tm <robert.chenault.jobs@gmail.com>
2019-06-06 15:19:30 -0400

    Added a class for managing which frontend to expect

---
+++ src/main/java/chenaurj/poefilterback/poefilterback/util/DeployConfig.java
@@ -0,0 +1,17 @@
+package chenaurj.poefilterback.poefilterback.util;
+
+
+public class DeployConfig {
+
+    private static String localCorsUrl = "http://localhost:4200";
+    private static String pcfCorsUrl = "https://chenaurj-poe-filter-front.cfapps.io";
+    
+    // Change me
+    private static String activeCorsUrl = pcfCorsUrl;
+
+    public static String getCorsUrl() {
+        return activeCorsUrl;
+    }
+
+    
+}
